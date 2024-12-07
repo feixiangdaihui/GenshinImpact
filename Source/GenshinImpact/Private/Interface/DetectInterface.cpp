85f1428bbc13d9cef721ffad0f9c7d481bbe5b64
Qingpeng Xu <2354281@tongji.edu.cn>
2024-12-07 21:34:25 +0800

    Revert "重构了敌人的三个组件"
    This reverts commit d7db6db5220529a16cc9393be75b975520d687c6.

--- Source/GenshinImpact/Private/Interface/DetectInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interface/DetectInterface.h"
-
-// Add default functionality here for any IDetectInterface functions that are not pure virtual.
--- Source/GenshinImpact/Public/Enemy/EnemyCharacter.h
+++ Source/GenshinImpact/Public/Enemy/EnemyCharacter.h
@@ -5,9 +5,6 @@
 #include "EnemyCharacter.generated.h"
 
 class UHealthComponent;
-class UAttackComponent;
-class UMoveComponent;
-class UDetectComponent;
 
 UCLASS()
 class GENSHINIMPACT_API AEnemyCharacter : public ACharacter
@@ -17,24 +14,134 @@ class GENSHINIMPACT_API AEnemyCharacter : public ACharacter
     friend class UEnemyAnim;
 
 protected://内部变量
-    // 血量组件
+    // 出生点位置
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+    FVector SpawnLocation;
+
+    // 血量属性
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float CurrentHealth;
+
+    // 最大血量
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float MaxHealth;
+
+    // 回血量
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float HealHealth;
+
+    // 攻击力
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float AttackPower;
+
+    // 攻击范围
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float AttackRange;
+
+    // 攻击间隔
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float AttackInterval;
+
+    // 走路速度
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float WalkSpeed;
+
+    // 跑步速度
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    float RunSpeed;
+
+    // 转身速度
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float RotationRate;
+
+    // 当前速度
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "State")
+    float CurrentSpeed;
+
+    // 是否正在攻击
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "State")
+    bool IsAttacking;
+
+    // 是否正在被攻击
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "State")
+    bool IsBeingAttacked;
+
+    // 是否死亡
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "State")
+    bool IsDead;
+
+    // 是否坐下
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "State")
+	bool IsSitting;
+
+    // 是否巡逻
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "State")
+	bool IsPatrol;
+
+    // 巡逻范围
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float PatrolRange;
+
+    // 感知范围
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float DetectionRange;
+
+    // 感知角度
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float DetectionAngle;
+
+	// 是否能看到玩家
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+	bool PlayerInSight;
+
+    // 定时器句柄
+    FTimerHandle HealTimerHandle;                // 用于回血的定时器
+    FTimerHandle ResetBeingAttackedTimerHandle;  // 重置被攻击状态的定时器
+    FTimerHandle ResetAttackingTimerHandle;      // 重置攻击状态的定时器
+	float SittingTimer;						  // 坐下时间
+	float PatrolTimer;                            // 巡逻时间
+
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Health")
     TObjectPtr<UHealthComponent> HealthComponent;
 
-    // 移动组件
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
-	TObjectPtr<class UMoveComponent> MoveComponent;
-
-    // 攻击组件
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attack")
-	TObjectPtr<class UAttackComponent> AttackComponent;
-	
-    // 感知组件
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Detection")
-    TObjectPtr<class UDetectComponent> DetectComponent;
-
 public://外部接口
+    // 构造函数
     AEnemyCharacter();
+
+    // 受伤函数
+    UFUNCTION(BlueprintCallable, Category = "Actions")
+    void TakeDamage(float DamageAmount);
+
+    // 攻击函数
+    UFUNCTION(BlueprintCallable, Category = "Actions")
+    void Attack();
+
+    // 死亡处理
+    UFUNCTION(BlueprintCallable, Category = "Actions")
+    void Die();
+
+    // 回血函数
+    UFUNCTION(BlueprintCallable, Category = "Actions")
+    void HealOverTime();
+
+    // 获取出生点位置
+    UFUNCTION(BlueprintCallable, Category = "Stats")
+    FVector GetSpawnLocation() const;
+
+    // 获取攻击范围
+    UFUNCTION(BlueprintCallable, Category = "Stats")
+    float GetAttackRange() const;
+
+	// 获取感知范围
+	UFUNCTION(BlueprintCallable, Category = "AI")
+	float GetDetectionRange() const;
+
+	// 获取上一次伤害方向
+	FVector GetLastDamageDirection() const;
+
+    //// 判断状态（静止，行走，跑步）
+    //UFUNCTION(BlueprintCallable, Category = "Actions")
+    //CharacterState GetCharacterState();
 
 protected:
     virtual void BeginPlay() override;
